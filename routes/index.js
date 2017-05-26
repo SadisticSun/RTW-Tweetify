78619bed84165b8484c9aba6730a76624252ca6a
SadisticSun <tristan@iamt.nl>
2017-05-26 15:24:26 +0200

    more changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const Router = express.Router();
 
 const client_id = '1eea3c72369f45a8908bc3d243d8acef';
 const client_secret = '732dd6b4f0a64bfb8c07a25838ed4b7b';
-const redirect_uri = 'https://rtw-tweetify.herokuapp.com/login';
+const redirect_uri = process.env.REDIRECT_URI;
 const scope = 'user-top-read user-read-private user-read-currently-playing user-read-playback-state';
 
 // Base Request URL
