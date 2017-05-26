4ac2f017334a8c13c76a44de0d9479543f3e53c6
SadisticSun <tristan@iamt.nl>
2017-05-26 02:36:23 +0200

    changed redirect_uri

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const Router = express.Router();
 
-// Spotify Keys
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-const redirect_url = process.env.REDIRECT_URI;
-const response_type = 'code';
-const grant_type = 'authorization_code';
+const client_id = '1eea3c72369f45a8908bc3d243d8acef';
+const client_secret = '732dd6b4f0a64bfb8c07a25838ed4b7b';
+const redirect_uri = 'https://rtw-tweetify.herokuapp.com/login';
 const scope = 'user-top-read user-read-private user-read-currently-playing user-read-playback-state';
-const redirect_uri = 'http://localhost:3000/login';
 
 // Base Request URL
 const base_URL = 'https://accounts.spotify.com/authorize/';
-var request_url = base_URL + '?client_id=' + client_id + '&scope=' + scope + '&response_type=' + response_type + '&redirect_uri=' + redirect_uri;
+var request_url = base_URL + '?client_id=' + client_id + '&scope=' + scope + '&response_type=code&redirect_uri=' + redirect_uri;
 console.log(request_url);
 
 Router.get('/', function(req, res) {
